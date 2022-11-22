a1992fa68b0a25d05af271a7327fa45de3d6525c
Nene <102773606+LizardoCt@users.noreply.github.com>
2022-11-22 18:33:03 -0500

    Add files via upload

---
+++ Card.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+
+const Card = (props) => {
+
+  if (props.media === "image"){
+    return(
+
+      <div className="container mt-5 pb-3 mb-5 d-flex justify-content-center">
+      <div className="card glass-card" style={{width: "40rem"}}>
+        <div className='card-body text-light'>
+
+            <h3 className='text-center mb-4 mt-2 font-monospace fw-bold'>{props.title}</h3>
+            <h5 className='title mb-3 text-center font-monospace'>{props.date}</h5>
+            <img className='img-fluid img-nasa rounded mx-auto d-block' src={props.url} alt={props.title} />
+            <h5 className='title mb-3 text-center font-monospace'>Photographed by: {props.copyright}</h5>
+            <p className='title mb-3 text-center font-monospace'>
+              <a className='api-url ' target='blank' href={props.hdurl}>{props.hdurl}</a>
+            </p>
+            <p className='fst-italic api-description'>{props.description}</p>
+
+        </div>
+      </div>
+    </div>
+
+    )
+  } else {
+
+    return(
+
+      <div className="container mt-5 pb-3 mb-5 d-flex justify-content-center">
+      <div className="card glass-card" style={{width: "40rem"}}>
+        <div className='card-body text-light'>
+
+            <h3 className='text-center mb-4 mt-2 font-monospace fw-bold'>{props.title}</h3>
+            <h5 className='title mb-3 text-center font-monospace'>{props.date}</h5>
+            <iframe className='img-fluid img-nasa rounded video' src={props.url} alt={props.title}></iframe>
+            <p className='title mb-3 text-center font-monospace'>
+              <a className='api-url ' target='blank' href={props.url}>{props.url}</a>
+            </p>
+            <p className='fst-italic api-description'>{props.description}</p>
+
+        </div>
+      </div>
+    </div>
+
+    )
+  }
+}
+
+export default Card
+// , backgroundColor: 'rgba(245, 245, 245, 0.301)'
---
+++ Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <div className='d-flex justify-content-center mt-5 text-light text-center'>
+        <p className='fw-bold font-monospace'>API:
+        <a className='api-url font-monospace' href="https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY" target='blank'>
+        https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY
+        </a>
+        </p>
+      </div>
+        <span className='d-flex justify-content-center mt-4 text-light'>Choose a date:</span>
+    </div>
+  )
+}
+
+export default Header
