af03ed7886c699c390f71dd8786b39d3962fd0a3
Nene <102773606+LizardoCt@users.noreply.github.com>
2023-01-02 21:39:59 -0500

    layout upgrade

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,13 +4,13 @@ const Header = () => {
   return (
     <div>
       <div className='d-flex justify-content-center mt-5 text-light text-center'>
-        <p className='fw-bold font-monospace'>API:
+        {/* <p className='fw-bold font-monospace'>API:
         <a className='api-url font-monospace' href="https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY" target='blank'>
         https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY
         </a>
-        </p>
+        </p> */}
       </div>
-        <span className='d-flex justify-content-center mt-4 text-light'>Choose a date:</span>
+        <span className='d-flex justify-content-center mt-4 api-url font-monospace'>Choose a new date:</span>
     </div>
   )
 }
