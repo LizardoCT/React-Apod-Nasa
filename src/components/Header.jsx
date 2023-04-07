70c73d8f6f9f4c8d07453f6473604a3479930167
Nene <102773606+LizardoCt@users.noreply.github.com>
2023-04-06 20:59:06 -0500

    ui updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,11 +4,6 @@ const Header = () => {
   return (
     <div>
       <div className='d-flex justify-content-center mt-5 text-light text-center'>
-        {/* <p className='fw-bold font-monospace'>API:
-        <a className='api-url font-monospace' href="https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY" target='blank'>
-        https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY
-        </a>
-        </p> */}
       </div>
         <span className='d-flex justify-content-center mt-4 api-url font-monospace'>Choose a new date:</span>
     </div>
