135623d61598e322b5396a95d27a61e86588d740
LizardoCt <lizardo_93@hotmail.com>
2024-09-02 15:55:13 -0500

    style: fix mobile breakpoint

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import gif from '../assets/images/nasagif.gif'
 const Navbar = () => {
   return (
     <div className='nav-bg glass-nav d-flex align-items-center justify-content-center'>
-      <img src={gif} className='nasa-logo nasa-gif' />
+      <img src={gif} className='nasa-gif' alt='nasa gif' />
       <h1 className='text-media text-light fs-2 fw-bold'>
         Astronomy Picture of the Day - NASA
       </h1>
